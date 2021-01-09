21d4bbfdc087bc762ad7f1ef5e94a5366738b5e5
Mr-Asad <asadalinawaz600@gmail.com>
2021-01-09 17:09:07 +0500

    Starting Vue App

--- ads.php
+++
@@ -1,5 +0,0 @@
-    <!-- ads -->
-    <section id="ads">
-        <p class="ads-p">"How to Optimize Your Sales Plays for 2021" with 6sense and Salesloft 🥳 <a href="#" class="a">Watch On-Demand!</a></p>
-    </section>
-    <!-- #ads -->
--- web-names.php
+++
@@ -1,5 +0,0 @@
-<!-- web-names -->
-    <div class="web-name">
-        
-    </div>
-<!-- #web-names -->
